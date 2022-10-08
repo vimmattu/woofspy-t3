306c9042b6dbead4664c8944343a45dd4f113042
joelkur <jkuronen95@gmail.com>
2022-10-09 00:48:07 +0300

    Add initial logic for creating recordings for sessions

--- src/pages/sessions.tsx
+++ src/pages/sessions.tsx
@@ -7,10 +7,15 @@ import { AppRouter } from "../server/trpc/router";
 
 const Sessions: NextPage = () => {
   const { data, isLoading } = trpc.sessions.getSessions.useQuery();
+  const { mutate } = trpc.sessions.createSession.useMutation();
 
   if (isLoading) {
     return <p>loading...</p>;
   }
+
+  const startSession = () => {
+    mutate();
+  };
 
   return (
     <>
@@ -19,6 +24,12 @@ const Sessions: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <button
+          className="rounder border-2 border-gray-500 bg-gray-500 text-white"
+          onClick={startSession}
+        >
+          Start session
+        </button>
         <ul>
           {data?.map((session) => (
             <SessionItem session={session} key={session.id} />
@@ -39,6 +50,11 @@ const SessionItem: React.FC<{ session: InferredSessionType }> = ({
   const { data } = trpc.sessions.getRecordings.useQuery({
     sessionId: session.id,
   });
+  const { mutate } = trpc.sessions.createRecording.useMutation();
+
+  const createRecording = () => {
+    mutate({ sessionId: session.id });
+  };
 
   return (
     <li>
@@ -47,6 +63,12 @@ const SessionItem: React.FC<{ session: InferredSessionType }> = ({
         {data &&
           data.map((recording) => <li key={recording.id}>- {recording.id}</li>)}
       </ul>
+      <button
+        className="rounder border-2 border-gray-500 bg-gray-500 text-white"
+        onClick={createRecording}
+      >
+        Create recording for {session.id}
+      </button>
     </li>
   );
 };
--- src/server/trpc/router/sessions.ts
+++ src/server/trpc/router/sessions.ts
@@ -5,11 +5,21 @@ export const sessionsRouter = t.router({
   getSessions: t.procedure.query(({ ctx }) => {
     return ctx.prisma.spySession.findMany();
   }),
+  createSession: t.procedure.mutation(({ ctx }) => {
+    return ctx.prisma.spySession.create({ data: {} });
+  }),
   getRecordings: t.procedure
     .input(z.object({ sessionId: z.string() }))
     .query(({ ctx, input }) => {
       return ctx.prisma.recording.findMany({
         where: { sessionId: input.sessionId },
+      });
+    }),
+  createRecording: t.procedure
+    .input(z.object({ sessionId: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.recording.create({
+        data: { sessionId: input.sessionId },
       });
     }),
 });
