5872471077c946ab521e56a1f956f2a4f77b2684
joelkur <jkuronen95@gmail.com>
2022-10-08 20:45:14 +0300

    Add login page logic

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,22 +1,28 @@
 import type { GetServerSideProps, NextPage } from "next";
-import { getProviders } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-type Props = {
-  providers: typeof getProviders
-}
+const SignIn: NextPage = () => {
+  const session = useSession();
+  const router = useRouter();
 
-const SignIn: NextPage<Props> = ({ providers }) => {
-  //console.log(providers)
-  Object.entries(providers).forEach(console.log)
-  return <div>Helloo!</div>;
+  if (session.status === "loading") return <p>loading...</p>;
+
+  if (session.status === "authenticated") {
+    router.push("/");
+    return null;
+  }
+
+  return (
+    <div>
+      <button
+        className="rounded border-2 border-gray-500"
+        onClick={() => signIn("github")}
+      >
+        Sign in with Github
+      </button>
+    </div>
+  );
 };
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {
-      providers: await getProviders()
-    },
-  };
-}
 
 export default SignIn;
