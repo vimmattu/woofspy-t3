299a59f33b67da190cfacc250c74a06f2014007a
joelkur <jkuronen95@gmail.com>
2022-10-09 12:42:01 +0300

    Add start button in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,13 @@ const Home: NextPage = () => {
         <title>Woofspy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex h-screen w-screen justify-center">
+      <div className="flex h-screen w-screen flex-col items-center">
         <Navigation />
-        <main></main>
+        <main className="flex h-full items-center">
+          <button className="h-64 w-64 rounded-full bg-gray-500 text-2xl text-white transition-colors hover:bg-gray-400">
+            Start session
+          </button>
+        </main>
       </div>
     </>
   );
