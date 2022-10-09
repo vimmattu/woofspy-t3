e37d9924f3b8fafece3d337d6af6a41f40154f93
joelkur <jkuronen95@gmail.com>
2022-10-09 12:31:19 +0300

    Add navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,40 @@
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+
+const Navigation = () => {
+  return (
+    <nav className="flex h-min space-x-8">
+      <NavItem type="link" href="/" text="Start" />
+      <NavItem type="link" href="/sessions" text="Past sessions" />
+      <NavItem type="button" onClick={() => signOut()} text="Sign out" />
+    </nav>
+  );
+};
+
+type NavLink = {
+  type: "link";
+  href: string;
+};
+
+type NavButton = {
+  type: "button";
+  onClick: () => void;
+};
+
+type NavItem = (NavLink | NavButton) & { text: string };
+
+const NavItem: React.FC<NavItem> = (props) => {
+  const className = "text-xl transition-colors hover:text-gray-500";
+
+  return props.type === "link" ? (
+    <Link href={props.href}>
+      <a className={className}>{props.text}</a>
+    </Link>
+  ) : (
+    <button onClick={props.onClick} className={className}>
+      {props.text}
+    </button>
+  );
+};
+
+export default Navigation;
