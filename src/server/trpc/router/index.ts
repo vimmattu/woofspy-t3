ea27955f2af26fedf24df65dfdcfceafa15be8f6
joelkur <jkuronen95@gmail.com>
2022-10-09 10:47:26 +0300

    Remove example resource

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,11 +1,9 @@
 // src/server/trpc/router/index.ts
 import { t } from "../trpc";
-import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
 import { sessionsRouter } from "./sessions";
 
 export const appRouter = t.router({
-  example: exampleRouter,
   auth: authRouter,
   sessions: sessionsRouter,
 });
