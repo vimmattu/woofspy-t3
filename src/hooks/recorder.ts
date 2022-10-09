e0a69026b74acf5afe5c321a000a7dd1f37c0ecb
joelkur <jkuronen95@gmail.com>
2022-10-09 23:40:55 +0300

    Fix reading media stream if camera not found

--- src/hooks/recorder.ts
+++ src/hooks/recorder.ts
@@ -11,23 +11,31 @@ export function useMediaDevices() {
   return devices;
 }
 
+function getConstraintForType(
+  devices: MediaDeviceInfo[],
+  kind: "videoinput" | "audioinput",
+  id?: string
+) {
+  const hasDevices = devices.filter((d) => d.kind === kind).length;
+  return !hasDevices ? false : id ? { deviceId: { exact: id } } : true;
+}
+
 export function useMediaStream(id?: string) {
+  const devices = useMediaDevices();
   const [stream, setStream] = useState<MediaStream>();
 
   useEffect(() => {
+    if (!devices.length) return;
     navigator.mediaDevices
       .getUserMedia({
-        video: id ? { deviceId: { exact: id } } : true,
-        audio: id ? { deviceId: { exact: id } } : true,
+        video: getConstraintForType(devices, "videoinput", id),
+        audio: getConstraintForType(devices, "audioinput", id) || true,
       })
       .then(setStream);
-  }, [id]);
+  }, [id, devices]);
 
-  useEffect(() => {
-    return () => {
-      stream?.getTracks().forEach((t) => t.stop());
-    };
-  }, [stream]);
+  // Stop each track of stream on unmount
+  useEffect(() => () => stream?.getTracks().forEach((t) => t.stop()), [stream]);
 
   return stream;
 }
