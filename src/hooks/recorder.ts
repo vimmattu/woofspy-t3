9b6a2321a927a339da1caf72a73ae1cc9a9bee04
joelkur <jkuronen95@gmail.com>
2022-10-10 00:34:21 +0300

    Remove unused import

--- src/hooks/recorder.ts
+++ src/hooks/recorder.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import ActivityDetector from "../utils/activity";
 import ActivityRecorder from "../utils/recorder";
 
