7732ede22f2af27ea6d2cca723c181d4f9752b29
joelkur <jkuronen95@gmail.com>
2022-10-09 13:11:12 +0300

    Add logic for making spysessions user specific

--- src/server/trpc/router/sessions.ts
+++ src/server/trpc/router/sessions.ts
@@ -1,24 +1,45 @@
 import { t, authedProcedure } from "../trpc";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { prisma } from "../../db/client";
+
+async function assertSessionBelongsToUser(userId: string, sessionId: string) {
+  const session = await prisma.spySession.findFirst({
+    where: { id: sessionId },
+  });
+  if (session?.userId !== userId) throw new TRPCError({ code: "UNAUTHORIZED" });
+  return true;
+}
 
 export const sessionsRouter = t.router({
-  getSessions: t.procedure.query(({ ctx }) => {
-    return ctx.prisma.spySession.findMany({ orderBy: { startTime: "desc" } });
+  getSessions: authedProcedure.query(({ ctx }) => {
+    return ctx.prisma.spySession.findMany({
+      where: { userId: ctx.session.user.id },
+      orderBy: { startTime: "desc" },
+    });
   }),
-  createSession: t.procedure.mutation(({ ctx }) => {
-    return ctx.prisma.spySession.create({ data: {} });
+  createSession: authedProcedure.mutation(({ ctx }) => {
+    return ctx.prisma.spySession.create({
+      data: {
+        userId: ctx.session.user.id,
+      },
+    });
   }),
-  getRecordings: t.procedure
+  getRecordings: authedProcedure
     .input(z.object({ sessionId: z.string() }))
-    .query(({ ctx, input }) => {
+    .query(async ({ ctx, input }) => {
+      await assertSessionBelongsToUser(ctx.session.user.id, input.sessionId);
+
       return ctx.prisma.recording.findMany({
         where: { sessionId: input.sessionId },
         orderBy: { startTime: "desc" },
       });
     }),
-  createRecording: t.procedure
+  createRecording: authedProcedure
     .input(z.object({ sessionId: z.string() }))
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
+      await assertSessionBelongsToUser(ctx.session.user.id, input.sessionId);
+
       return ctx.prisma.recording.create({
         data: { sessionId: input.sessionId },
       });
