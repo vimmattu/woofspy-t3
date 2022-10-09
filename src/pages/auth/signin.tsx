f5cefae4f9952f85f1d43cff388602d57f3a7bef
joelkur <jkuronen95@gmail.com>
2022-10-09 16:17:51 +0300

    Add initial login page styling

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -8,14 +8,17 @@ const SignIn: NextPage = () => {
   if (!shouldRender) return null;
 
   return (
-    <div>
-      <button
-        className="rounded border-2 border-gray-500"
-        onClick={() => signIn("github")}
-      >
-        Sign in with Github
-      </button>
-    </div>
+    <main className="flex h-full items-center justify-center">
+      <div className="w-96 rounded border-2 border-gray-100 bg-white p-4 shadow-md">
+        <h1 className="mb-2 text-center text-2xl">Sign in</h1>
+        <button
+          className="w-full rounded bg-gray-500 p-2 text-center text-white transition-colors hover:bg-gray-600 focus:bg-gray-600"
+          onClick={() => signIn("github")}
+        >
+          Github
+        </button>
+      </div>
+    </main>
   );
 };
 
