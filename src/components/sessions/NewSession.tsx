ec861da0771de9ec03ff351d9625d03a55062cc0
joelkur <jkuronen95@gmail.com>
2022-10-09 22:02:14 +0300

    Add new session styling

--- src/components/sessions/NewSession.tsx
+++ src/components/sessions/NewSession.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { useMediaStream } from "../../hooks/recorder";
+import { useEndSession } from "../../hooks/sessions";
 import { InferredSessionType } from "./types";
 
 // TODO: Confirm page leave from user
@@ -8,7 +9,8 @@ interface Props {
   session: InferredSessionType;
 }
 
-const NewSession: React.FC<Props> = () => {
+const NewSession: React.FC<Props> = ({ session }) => {
+  const { mutate: endSession, isLoading } = useEndSession();
   const videoRef = useRef<HTMLVideoElement>(null);
   const stream = useMediaStream();
 
@@ -18,8 +20,16 @@ const NewSession: React.FC<Props> = () => {
   }, [stream]);
 
   return (
-    <div>
-      <video ref={videoRef} autoPlay muted />
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="mb-2 text-center text-3xl">Woof spy</h1>
+      <video ref={videoRef} autoPlay muted className="mb-2" />
+      <button
+        className="padding-2 rounded bg-red-600 px-4 py-2 text-xl text-white shadow transition-colors hover:bg-red-700 focus:bg-red-700"
+        onClick={() => endSession({ id: session.id })}
+        disabled={isLoading}
+      >
+        End spy
+      </button>
     </div>
   );
 };
