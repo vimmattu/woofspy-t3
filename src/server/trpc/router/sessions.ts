4fdb9771bb7b975fc5f0b6e968fe7d415116c4d0
joelkur <jkuronen95@gmail.com>
2022-10-09 10:42:32 +0300

    Change sessions and recordings list ordering

--- src/server/trpc/router/sessions.ts
+++ src/server/trpc/router/sessions.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const sessionsRouter = t.router({
   getSessions: t.procedure.query(({ ctx }) => {
-    return ctx.prisma.spySession.findMany();
+    return ctx.prisma.spySession.findMany({ orderBy: { startTime: "desc" } });
   }),
   createSession: t.procedure.mutation(({ ctx }) => {
     return ctx.prisma.spySession.create({ data: {} });
@@ -13,6 +13,7 @@ export const sessionsRouter = t.router({
     .query(({ ctx, input }) => {
       return ctx.prisma.recording.findMany({
         where: { sessionId: input.sessionId },
+        orderBy: { startTime: "desc" },
       });
     }),
   createRecording: t.procedure
