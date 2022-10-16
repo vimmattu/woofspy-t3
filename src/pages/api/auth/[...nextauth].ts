dd9ded617b6860863a556c5c8babdfaaa1f83ece
joelkur <jkuronen95@gmail.com>
2022-10-16 20:03:05 +0300

    Add credentials provider and ui for login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,13 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 import { env } from "../../../env/server.mjs";
+import { verify } from "argon2";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -30,6 +32,30 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: env.GCP_CLIENT_ID,
       clientSecret: env.GCP_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { type: "email", label: "Email" },
+        password: { type: "password", label: "Password" },
+      },
+      authorize: async (credentials, _) => {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const user = await prisma.user.findFirst({ where: { email } });
+        if (!user) return null;
+        if (!user.password) return null;
+
+        const passwordValid = verify(user.password, password);
+
+        if (!passwordValid) return null;
+        return {
+          id: user.id,
+          email: user.email,
+        };
+      },
     }),
     // ...add more providers here
   ],
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,7 +1,19 @@
 import { signIn } from "next-auth/react";
 import Head from "next/head";
+import { FormEventHandler, useRef } from "react";
 
 const SignIn = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    signIn("credentials", {
+      email: emailRef.current?.value,
+      password: passwordRef.current?.value,
+    });
+  };
+
   return (
     <>
       <Head>
@@ -11,8 +23,35 @@ const SignIn = () => {
       <main className="flex h-full items-center justify-center">
         <div className="w-96 rounded border-2 border-gray-100 bg-white p-4 shadow-md">
           <h1 className="mb-2 text-center text-2xl">Sign in</h1>
+          <form
+            className="mb-4  border-b-2 border-b-gray-200"
+            onSubmit={onSubmit}
+          >
+            <label htmlFor="email">Email</label>
+            <input
+              ref={emailRef}
+              id="email"
+              type="email"
+              className="mb-2 block w-full border-2 border-gray-200"
+              placeholder="user@example.com"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              ref={passwordRef}
+              id="password"
+              type="password"
+              className="mb-2 block w-full border-2 border-gray-200"
+              placeholder="********"
+            />
+            <button
+              type="submit"
+              className="mb-4 w-full rounded bg-gray-500 p-2 text-center text-white transition-colors hover:bg-gray-600 focus:bg-gray-600"
+            >
+              Sign in
+            </button>
+          </form>
           <button
-            className="w-full rounded bg-gray-500 p-2 text-center text-white transition-colors hover:bg-gray-600 focus:bg-gray-600"
+            className="mb-2 w-full rounded bg-gray-500 p-2 text-center text-white transition-colors hover:bg-gray-600 focus:bg-gray-600"
             onClick={() => signIn("github")}
           >
             Github
