06d4ff81840a56af51e0080b1996bca03a1a607a
joelkur <jkuronen95@gmail.com>
2022-10-16 21:20:37 +0300

    Replace createSession with redirect to spy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useCreateSession } from "../hooks/sessions";
 
 const Home: NextPage = () => {
-  const { mutate: createSession, isLoading } = useCreateSession();
+  const router = useRouter();
 
   // Display spinner if fetching user data or active session is in loading state,
   // or if an active session is found. (If active session is found, then useActiveSession redirects user to session details)
@@ -18,9 +19,8 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex h-full items-center">
         <button
-          disabled={isLoading}
           className="h-56 w-56 rounded-full bg-red-600 text-2xl text-white shadow-lg transition-colors hover:bg-red-700 focus:bg-red-700"
-          onClick={() => createSession()}
+          onClick={() => router.push("/spy")}
         >
           Start session
         </button>
