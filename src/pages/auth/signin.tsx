faf54e00c14e05cadcdab8d3e2a0bb66819e01a5
joelkur <jkuronen95@gmail.com>
2022-10-16 21:22:03 +0300

    Add spacing between auth provider buttons

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -12,7 +12,7 @@ const SignIn = () => {
         <div className="w-96 rounded border-2 border-gray-100 bg-white p-4 shadow-md">
           <h1 className="mb-2 text-center text-2xl">Sign in</h1>
           <button
-            className="w-full rounded bg-gray-500 p-2 text-center text-white transition-colors hover:bg-gray-600 focus:bg-gray-600"
+            className="mb-2 w-full rounded bg-gray-500 p-2 text-center text-white transition-colors hover:bg-gray-600 focus:bg-gray-600"
             onClick={() => signIn("github")}
           >
             Github
