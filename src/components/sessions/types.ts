3789a5b95357966cf1f03df75315e8d0ebeec1c3
joelkur <jkuronen95@gmail.com>
2022-10-18 01:16:46 +0300

    Remove old implementation and ideas

--- src/components/sessions/types.ts
+++
@@ -1,6 +0,0 @@
-import { inferProcedureOutput } from "@trpc/server";
-import { AppRouter } from "../../server/trpc/router";
-
-export type InferredSessionType = NonNullable<
-  inferProcedureOutput<AppRouter["sessions"]["getSession"]>
->;
