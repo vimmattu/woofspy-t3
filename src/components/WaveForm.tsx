e491a988a4e2323d773076ee05884b53c30e2da4
joelkur <jkuronen95@gmail.com>
2022-10-18 00:01:42 +0300

    Refactor waveform to separate file

---
+++ src/components/WaveForm.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from "react";
+import WaveSurfer from "wavesurfer.js";
+
+const WaveForm = ({ stream }: { stream?: MediaStream }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current || !stream) return;
+
+    const audioContext = new AudioContext();
+    const source = audioContext.createMediaStreamSource(stream);
+    const audioScriptProcessor = audioContext.createScriptProcessor(4096, 1, 1);
+
+    const ws = WaveSurfer.create({
+      container: containerRef.current,
+      audioContext,
+      audioScriptProcessor,
+      barWidth: 2,
+      interact: false,
+    });
+
+    source.connect(audioScriptProcessor);
+    audioScriptProcessor.connect(audioContext.destination);
+
+    audioScriptProcessor.onaudioprocess = (event) => {
+      ws.empty();
+      ws.loadDecodedBuffer(event.inputBuffer);
+    };
+
+    return () => {
+      ws.destroy();
+      source.disconnect();
+      audioScriptProcessor.disconnect();
+      audioScriptProcessor.onaudioprocess = undefined;
+    };
+  }, [stream]);
+
+  return <div className="w-full" ref={containerRef} />;
+};
+
+export default WaveForm;
--- src/components/spy/SelectMicrophone.tsx
+++ src/components/spy/SelectMicrophone.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { useMediaDevices } from "../../hooks/devices";
+import WaveForm from "../WaveForm";
 import { BaseDeviceAskProps } from "./types";
-import WaveSurfer from "wavesurfer.js";
 
 const MicrophoneSelection: React.FC<BaseDeviceAskProps> = (props) => {
   const displayInfo = !props.stream && !props.error;
@@ -83,43 +83,6 @@ const MicrophonePreview: React.FC<BaseDeviceAskProps> = ({
       </button>
     </>
   );
-};
-
-const WaveForm = ({ stream }: { stream?: MediaStream }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!containerRef.current || !stream) return;
-
-    const audioContext = new AudioContext();
-    const source = audioContext.createMediaStreamSource(stream);
-    const audioScriptProcessor = audioContext.createScriptProcessor(4096, 1, 1);
-
-    const ws = WaveSurfer.create({
-      container: containerRef.current,
-      audioContext,
-      audioScriptProcessor,
-      barWidth: 2,
-      interact: false,
-    });
-
-    source.connect(audioScriptProcessor);
-    audioScriptProcessor.connect(audioContext.destination);
-
-    audioScriptProcessor.onaudioprocess = (event) => {
-      ws.empty();
-      ws.loadDecodedBuffer(event.inputBuffer);
-    };
-
-    return () => {
-      ws.destroy();
-      source.disconnect();
-      audioScriptProcessor.disconnect();
-      audioScriptProcessor.onaudioprocess = undefined;
-    };
-  }, [stream]);
-
-  return <div className="w-full" ref={containerRef} />;
 };
 
 export default MicrophoneSelection;
