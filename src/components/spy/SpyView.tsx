2926c4023bcdd36e183773adb457c46e9d537ccf
joelkur <jkuronen95@gmail.com>
2022-10-18 00:06:30 +0300

    Hide video element in spy view if camera not in use

--- src/components/spy/SpyView.tsx
+++ src/components/spy/SpyView.tsx
@@ -3,10 +3,12 @@ import WaveForm from "../WaveForm";
 import { BaseProps } from "./types";
 
 const SpyView: React.FC<BaseProps> = ({ stream, error, proceedSetup }) => {
+  const hasVideoTracks = !!stream?.getTracks().filter((d) => d.kind === "video")
+    .length;
   return (
     <>
       <h1 className="mb-2 text-center text-2xl">Spy</h1>
-      <Video stream={stream} />
+      {hasVideoTracks && <Video stream={stream} />}
       <WaveForm stream={stream} />
     </>
   );
