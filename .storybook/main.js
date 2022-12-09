0169a5d9629303a83d4db3d80649191e3a1ad844
joelkur <jkuronen95@gmail.com>
2022-12-10 00:07:27 +0200

    Initial replace tailwind with chakraui

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+const path = require("path");
+module.exports = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@chakra-ui/storybook-addon",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    docsPage: true,
+  },
+  features: {
+    emotionAlias: false,
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,26 @@
 // src/pages/_app.tsx
-import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 import Layout from "../components/Layout";
 import { AuthGuard } from "../components/AuthGuard";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <AuthGuard>
-          <Component {...pageProps} />
-        </AuthGuard>
-      </Layout>
-    </SessionProvider>
+    <ChakraProvider>
+      <SessionProvider session={session}>
+        <Layout>
+          <AuthGuard>
+            <Component {...pageProps} />
+          </AuthGuard>
+        </Layout>
+      </SessionProvider>
+    </ChakraProvider>
   );
 };
 
