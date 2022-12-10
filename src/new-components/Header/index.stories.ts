a04e74d113308142192fbcfc0429de52fae9000c
joelkur <jkuronen95@gmail.com>
2022-12-10 12:46:33 +0200

    Change header storybook title

--- src/new-components/Header/index.stories.ts
+++ src/new-components/Header/index.stories.ts
@@ -4,7 +4,7 @@ import { Header } from "./";
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta: Meta<typeof Header> = {
-  title: "Woofspy/Header",
+  title: "Components/Header",
   component: Header,
   tags: ["docsPage"],
 };
