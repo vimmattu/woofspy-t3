d82773669ff1587b434e27a9418949719d2d233e
joelkur <jkuronen95@gmail.com>
2022-12-11 23:13:44 +0200

    Change tab item to be active on route path match

--- src/new-components/components/NavigationTabs/index.tsx
+++ src/new-components/components/NavigationTabs/index.tsx
@@ -1,6 +1,7 @@
 import { Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 interface Tab {
   href: string;
@@ -30,7 +31,11 @@ export const NavigationTabs = ({ children }: Props) => {
   );
 };
 
-export const TabItem = ({ href, title, active }: Tab) => {
+export const TabItem = ({ href, title }: Tab) => {
+  const router = useRouter();
+
+  const active = router.pathname === href;
+
   return (
     <Link
       as={NextLink}
@@ -40,8 +45,8 @@ export const TabItem = ({ href, title, active }: Tab) => {
       w="full"
       borderRadius="sm"
       borderBottomRadius="none"
-      bg={active ? "gray.50" : "none"}
-      _hover={{ textDecoration: "none", bg: "gray.50" }}
+      bg={active ? "gray.100" : "none"}
+      _hover={{ textDecoration: "none", bg: "gray.100" }}
       p={2}
     >
       {title}
