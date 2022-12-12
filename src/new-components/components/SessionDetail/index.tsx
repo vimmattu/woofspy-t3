e41f98a34c2ae93ff8e15004ec37f6dac7d3adfa
joelkur <jkuronen95@gmail.com>
2022-12-12 22:45:30 +0200

    Add scaffolding for session details component

---
+++ src/new-components/components/SessionDetail/index.tsx
@@ -0,0 +1,34 @@
+import {
+    Box,
+  Button,
+  Flex,
+  LinkBox,
+  LinkOverlay,
+  Spacer,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import NextLink from "next/link";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import dayjs from "dayjs";
+import type { Session } from '../../../types/inferred'
+
+interface Props {
+  session: Session
+}
+
+const formatDate = (date: Date) => dayjs(date).format("DD:MM:YYYY");
+const formatTime = (date: Date) => dayjs(date).format("HH:mm:ss");
+
+export const SessionDetail = ({ session }: Props) => {
+  return (
+    <Box>
+      <Flex>
+        <Text>{formatDate(session.startTime)}</Text>
+        <Text>{formatTime(session.startTime)} - {session.endTime ? formatTime(session.endTime): ''}</Text>
+      </Flex>
+      
+    </Box>
+  );
+};
