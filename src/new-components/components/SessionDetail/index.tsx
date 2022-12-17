78ad1696203226d70f6d920ff4275499c9817436
joelkur <jkuronen95@gmail.com>
2022-12-17 23:12:54 +0200

    Add session detail component event details and dropdown button

--- src/new-components/components/SessionDetail/index.tsx
+++ src/new-components/components/SessionDetail/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Divider, Flex, Text, VStack } from "@chakra-ui/react";
-import React from "react";
-import { CalendarIcon } from "@chakra-ui/icons";
+import { Box, Divider, Flex, IconButton, Text, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { CalendarIcon, ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import dayjs from "dayjs";
 import type { Session } from "../../../types/inferred";
 
@@ -40,15 +40,26 @@ interface RecordingItemProps {
 }
 
 const RecordingItem = ({ isLast }: RecordingItemProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
     <Flex minH={isLast ? 12 : 16} w="full">
       <Box ml={8}>
         <CalendarIcon boxSize={8} ml="-16px" zIndex={1} bg="white" />
         <Divider opacity={1} orientation="vertical" />
       </Box>
-      <Box>
-        <Text>Recorded event</Text>
-      </Box>
+      <Flex justifyContent="space-between" ml={2} mt={1.5} w="full">
+        <Text>16:52 - Recorded event</Text>
+        <IconButton
+          aria-label="Expand event"
+          icon={isExpanded ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          h="min"
+          w="min"
+          minW={0}
+          variant="unstyled"
+          size="lg"
+          onClick={() => setIsExpanded((prev) => !prev)}
+        />
+      </Flex>
     </Flex>
   );
 };
