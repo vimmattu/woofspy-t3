a6fced9f70d10ed3cd95b0a5f977f6d80febb808
joelkur <jkuronen95@gmail.com>
2023-01-07 21:23:40 +0200

    Fix sectionlist not using keys in iterating sessions

--- src/new-components/components/SectionList/index.tsx
+++ src/new-components/components/SectionList/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Text } from "@chakra-ui/react";
 import { SessionCard } from "../SessionCard";
 import type { Session } from "../../../types/inferred";
+import { Fragment } from "react";
 
 interface Props {
   sessions: Record<string, Session[]>;
@@ -10,7 +11,7 @@ export const SessionList = ({ sessions }: Props) => {
   return (
     <Box w="full">
       {Object.entries(sessions).map(([date, sessions]) => (
-        <>
+        <Fragment key={date}>
           <Text fontWeight="bold" fontSize="xl" mb={2}>
             {date}
           </Text>
@@ -23,7 +24,7 @@ export const SessionList = ({ sessions }: Props) => {
               key={s.id}
             />
           ))}
-        </>
+        </Fragment>
       ))}
     </Box>
   );
