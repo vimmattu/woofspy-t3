902a0b89e929062421f6662ddccfb3b81d1386ea
joelkur <jkuronen95@gmail.com>
2023-01-07 23:02:06 +0200

    Add render logic in home page when session is active

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Link, Button, Heading, VStack, Spinner } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import NextLink from "next/link";
-import { useSessions } from "../hooks/sessions";
+import { useActiveSession, useSessions } from "../hooks/sessions";
 import { Head } from "../new-components/components/Head";
 import { SessionList } from "../new-components/components/SectionList";
 
@@ -12,32 +12,50 @@ const Home: NextPage = () => {
   // or if an active session is found. (If active session is found, then useActiveSession redirects user to session details)
   // TODO: Replace null with spinner
   // if (!shouldRender || isFetchingActiveSession || !!activeSession) return null;
+  const { data: activeSession, isLoading: isFetchingActiveSession } =
+    useActiveSession();
+
+  if (isFetchingActiveSession) return <Spinner />;
 
   return (
     <>
       <VStack as="main" mt={4}>
         <Head title="Dashboard" hasHiddenHeader />
-        {/*<Button as={NextLink} href="/spy" colorScheme="green" w="full">
-          Start spy
-        </Button>*/}
 
-        <Button
-          as={NextLink}
-          href="/spy"
-          colorScheme="red"
-          w="40"
-          h="40"
-          borderRadius="full"
-          fontSize="xl"
-          shadow="lg"
-          m={8}
-        >
-          Start spy
-        </Button>
-
-        {/* <Button as={NextLink} href="/spy" colorScheme="red" variant='outline' borderWidth='medium' w="40" h='40' borderRadius='full' fontSize='xl' shadow='lg' m={8} color='black' borderColor='red.600' bg='red.50'>
-          View spy 
-        </Button> */}
+        {activeSession ? (
+          <Button
+            as={NextLink}
+            href="/spy"
+            colorScheme="red"
+            variant="outline"
+            borderWidth="medium"
+            w="40"
+            h="40"
+            borderRadius="full"
+            fontSize="xl"
+            shadow="lg"
+            m={8}
+            color="black"
+            borderColor="red.600"
+            bg="red.50"
+          >
+            View spy
+          </Button>
+        ) : (
+          <Button
+            as={NextLink}
+            href="/spy"
+            colorScheme="red"
+            w="40"
+            h="40"
+            borderRadius="full"
+            fontSize="xl"
+            shadow="lg"
+            m={8}
+          >
+            Start spy
+          </Button>
+        )}
 
         <Heading as="h2" w="full" size="lg">
           Past sessions
