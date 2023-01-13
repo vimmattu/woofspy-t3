b7fbd2b1adf4008ea1ff2c0354238ae38cbc66dc
joelkur <jkuronen95@gmail.com>
2023-01-13 12:42:39 +0200

    Fix comic neue font not working

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,12 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
   fonts: {
-    heading: `'Comic Neue', 'cursive`,
-    body: `'Comic Neue', 'cursive`,
-  },
-  fontWeights: {
-    light: 300,
-    normal: 400,
-    bold: 700,
+    heading: `"Comic Neue", cursive`,
+    body: `"Comic Neue", cursive`,
   },
 });
