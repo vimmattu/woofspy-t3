771027aec35878bc6cda402048972f75defa75e3
joelkur <jkuronen95@gmail.com>
2023-01-13 13:02:28 +0200

    Improve invitation endpoint

--- src/pages/api/invitation/index.ts
+++ src/pages/api/invitation/index.ts
@@ -1,57 +1,56 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getServerAuthSession } from "../../../server/common/get-server-auth-session";
 import { prisma } from "../../../server/db/client";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+  const session = await getServerAuthSession({ req, res });
+  if (!session || !session.user) return res.redirect("/auth/signin");
 
-  if (typeof req.query.token !== "string") {
+  if (req.method !== "GET")
+    return res.status(405).json({ error: "Method not allowed" });
+  if (typeof req.query.token !== "string")
     return res.status(400).json({ error: "Bad request" });
-  }
 
   const invitation = await prisma.invitationToGroup.findFirst({
     where: {
       token: req.query.token,
     },
   });
-
-  if (!invitation) {
-    return res.status(404).json({ error: "Not found" });
-  }
-
-  if (invitation.expires < new Date()) {
+  if (!invitation) return res.status(404).json({ error: "Not found" });
+  if (invitation.expires < new Date())
     return res.status(400).json({ error: "Invitation expired" });
-  }
 
-  let user = await prisma.user.findFirst({
+  const user = await prisma.user.findFirst({
     where: {
       email: invitation.email,
     },
   });
 
-  if (!user) {
-    user = await prisma.user.create({
-      data: {
-        email: invitation.email,
+  if (user?.id !== session.user.id)
+    return res.status(403).json({ error: "Forbidden" });
+
+  await Promise.all([
+    prisma.group.update({
+      where: {
+        id: invitation.groupId,
       },
-    });
-  }
-
-  if (!user) return res.status(500).json({ error: "Internal server error" });
-
-  await prisma.group.update({
-    where: {
-      id: invitation.groupId,
-    },
-    data: {
-      users: {
-        create: {
-          userId: user.id,
+      data: {
+        users: {
+          create: {
+            userId: user.id,
+          },
         },
       },
-    },
-  });
+    }),
+    prisma.invitationToGroup.update({
+      where: {
+        token: invitation.token,
+      },
+      data: {
+        expires: new Date(),
+      },
+    }),
+  ]);
 
   return res.redirect("/");
 };
