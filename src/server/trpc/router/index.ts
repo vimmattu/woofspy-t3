39097fbccb1bfdc1839ebb908ddbf0531e23480a
joelkur <jkuronen95@gmail.com>
2023-01-15 01:12:31 +0200

    Remove unused auth route from trpc

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,12 +1,10 @@
 // src/server/trpc/router/index.ts
 import { t } from "../trpc";
-import { authRouter } from "./auth";
 import { groupsRouter } from "./groups";
 import { spySessionsRouter } from "./spysession.route";
 import { recordingsRouter } from "./recording.route";
 
 export const appRouter = t.router({
-  auth: authRouter,
   spySessions: spySessionsRouter,
   recordings: recordingsRouter,
   groups: groupsRouter,
