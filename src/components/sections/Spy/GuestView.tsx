b09d6ba77b16824d23a7f4df4b294391c6d01fcb
joelkur <jkuronen95@gmail.com>
2023-04-04 22:04:25 +0300

    Fix guest view video muted

--- src/components/sections/Spy/GuestView.tsx
+++ src/components/sections/Spy/GuestView.tsx
@@ -11,7 +11,7 @@ const GuestView: React.FC<{ sessionId: string }> = ({ sessionId }) => {
   return (
     <>
       <Head title="Spy" />
-      <Video stream={stream} muted />
+      <Video stream={stream} />
       <WaveForm stream={stream} />
     </>
   );
